68cbd7a71eb77d12d375d9fbdbd9478613eaf4f2
Badreddine Abbaoui <badreddine.abb@gmail.com>
2025-04-27 12:42:24 +0200

    Añadida opción para ordenar ascendente o descendente desde línea de comandos

--- main.py
+++ main.py
@@ -24,12 +24,17 @@ def remove_duplicates_from_list(items):
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
     remove_duplicates = DEFAULT_DUPLICATES
-    if len(sys.argv) == 3:
+    ascending = True
+
+    if len(sys.argv) == 4:
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
+        ascending = sys.argv[3].lower() == "asc"
     else:
-        print("Se debe indicar el fichero como primer argumento")
-        print("El segundo argumento indica si se quieren eliminar duplicados")
+        print("Uso: python3 main.py <filename> <dup> <order>")
+        print(" - filename: ruta del fichero de palabras")
+        print(" - dup: yes para eliminar duplicados, no para mantener")
+        print(" - order: asc para ordenar ascendente, desc para descendente")
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
@@ -46,4 +51,4 @@ if __name__ == "__main__":
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
 
-    print(sort_list(word_list))
+    print(sort_list(word_list, ascending=ascending))
